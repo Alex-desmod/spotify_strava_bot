132b34610ea34631e9562bb0374dfeac99e4cbbb
Alex Gusev <alex.desmod@gmail.com>
2026-02-24 09:28:42 +0300

    a little fix of config.py

--- bot_strava/config.py
+++ bot_strava/config.py
@@ -1,7 +1,9 @@
 import os
 
+from dotenv import load_dotenv
 from web_server.config import STRAVA_REDIRECT_URI
 
+load_dotenv()
 
 STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
 STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
