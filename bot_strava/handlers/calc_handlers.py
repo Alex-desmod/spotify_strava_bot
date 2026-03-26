049c0c1601836dfac5f0d6f0d9c8389ca0a4d95f
Alex Gusev <alex.desmod@gmail.com>
2026-03-26 21:00:05 +0300

    handle_pace in the making

--- bot_strava/handlers/calc_handlers.py
+++ bot_strava/handlers/calc_handlers.py
@@ -1,9 +1,12 @@
 import json
 import logging
+from datetime import datetime
 
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 
 import bot_strava.keyboards as kb
 
@@ -14,6 +17,10 @@ logger = logging.getLogger(__name__)
 
 with open("bot_strava/messages.json", "r", encoding="utf-8") as file:
     messages = json.load(file)
+
+
+class CalcState(StatesGroup):
+    waiting_for_pace = State()
 
 
 @router.message(Command("calc"))
@@ -29,7 +36,38 @@ async def result_time(message: Message):
 
 
 @router.message(Command("pace"))
-async def result_time(message: Message):
+async def pace_time(message: Message):
     await message.answer(messages[0]["distance"],
                          reply_markup=await kb.distance_pace()
                          )
+
+
+@router.callback_query(F.data.startswith("pace_"))
+async def pace_input(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    distance = int(callback.data.split("_")[1])
+    await state.set_state(CalcState.waiting_for_pace)
+    await state.update_data(
+        distance=distance,
+        pace_digits="",
+        timestamp=datetime.now()
+    )
+    await callback.message.answer("Введи темп. 3 цифры в формате x:xx мин/км",
+                                  reply_markup=await kb.numpad()
+                                  )
+
+
+@router.callback_query(CalcState.waiting_for_pace, F.data.in_([str(n) for n in range(10)]))
+async def handle_pace(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+
+    # timeout check (15 seconds)
+    elapsed = (datetime.now() - data["timestamp"]).total_seconds()
+    if elapsed > 15:
+        await state.clear()
+        return
+
+    digits = data.get("pace_digits", "") + callback.data
+
+    if len(digits) < 3:
+        await state.update_data(pace_digits=digits, timestamp=datetime.now())
--- bot_strava/keyboards.py
+++ bot_strava/keyboards.py
@@ -1,4 +1,5 @@
 import logging
+from dataclasses import dataclass
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from enum import Enum
@@ -9,14 +10,19 @@ from bot_strava.config import get_auth_link
 logger = logging.getLogger(__name__)
 
 
-class Distance(Enum):
-    K3 = "3 км"
-    K5 = "5 км"
-    K10 = "10 км"
-    M10 = "16,1 км"
-    HALF = "21,1 км"
-    K30 = "30 км"
-    MARATHON = "42,2 км"
+@dataclass
+class Distance:
+    label: str
+    meters: int
+
+class DistanceEnum(Enum):
+    K3 = Distance("3 км", 3000)
+    K5 = Distance("5 км", 5000)
+    K10 = Distance("10 км", 10000)
+    M10 = Distance("16,1 км", 16100)
+    HALF = Distance("21,1 км", 21097)
+    K30 = Distance("30 км", 30000)
+    MARATHON = Distance("42,2 км", 42195)
 
 
 async def authorize(tg_id):
@@ -36,15 +42,23 @@ async def start():
 
 async def distance_result():
     kb = InlineKeyboardBuilder()
-    for d in Distance:
-        kb.add(InlineKeyboardButton(text=d.value, callback_data=f"result_{d.name}"))
+    for d in DistanceEnum:
+        kb.add(InlineKeyboardButton(text=d.value.label, callback_data=f"result_{d.value.meters}"))
     return kb.adjust(3).as_markup()
 
 
 async def distance_pace():
     kb = InlineKeyboardBuilder()
-    for d in Distance:
-        kb.add(InlineKeyboardButton(text=d.value, callback_data=f"pace_{d.name}"))
+    for d in DistanceEnum:
+        kb.add(InlineKeyboardButton(text=d.value.label, callback_data=f"pace_{d.value.meters}"))
+    return kb.adjust(3).as_markup()
+
+
+async def numpad():
+    kb = InlineKeyboardBuilder()
+    for n in range(1,10):
+        kb.add(InlineKeyboardButton(text=str(n), callback_data=str(n)))
+    kb.add(InlineKeyboardButton(text="0", callback_data="0"))
     return kb.adjust(3).as_markup()
 
 
